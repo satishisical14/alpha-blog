d59566cc068f1f1ad86f72313dea10d697b1fda5
satishisical <satish.isical14@gmail.com>
2019-04-07 20:28:32 +0530

    prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
 	def new
 		@article = Article.new
 	end
@@ -44,6 +45,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+	  def set_article
+	  	@article = Article.find(params[:id])
+	  end
+
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
