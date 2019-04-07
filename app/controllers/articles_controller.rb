eb1d798933005d65319abf3b108e2a0929094019
satishisical <satish.isical14@gmail.com>
2019-04-07 20:23:41 +0530

    adds destroy and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,14 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit 
+		@article = Article.find(params[:id])
+	end
+
+	def index 
+		@article = Article.all 
+	end
+
 	def create
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
@@ -14,8 +22,25 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article updated succesfully"
+			redirect_to article_path(@article)
+		else
+			render :edit 
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article succesfully deleted"
+		redirect_to articles_path
 	end
 
 	private
