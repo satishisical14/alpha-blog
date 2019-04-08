b73f25b6a4d48c9c67c6b01ddd28acaaaf1d586a
satishisical <satish.isical14@gmail.com>
2019-04-09 00:25:27 +0530

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+	validates :username, presence: true, 
+						uniqueness: { case_sensitive: false }, 
+						length: { minimum: 3, maximum: 10 }
+	VALID_EMAIL_REGEX = /([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})/i
+	validates :email, presence: true, 
+						uniqueness: { case_sensitive: false }, 
+						length: { maximum: 10 },
+						format: { with: VALID_EMAIL_REGEX }
 end
