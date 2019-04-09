165ed44ee2083bbdde48998728552f1bf87e8bf9
satishisical <satish.isical14@gmail.com>
2019-04-09 22:28:05 +0530

    adds edit update to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,22 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit 
+		@user = User.find(params[:id])
+	end
+
 	def show
 		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "User updated succesfully"
+			redirect_to user_path(@user)
+		else
+			render :edit 
+		end
 	end
 
 	private
