c7e78cbb59a6a4bfd521b085584a202a18981442
satishisical <satish.isical14@gmail.com>
2019-04-09 22:18:38 +0530

    create user

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # get 'welcome/home', to: 'welcome#home'
   get 'about', to: 'welcome#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
