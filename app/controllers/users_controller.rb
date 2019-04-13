e15709c20674cb8fc6f606f97ee3f2d503461ffb
satishisical <satish.isical14@gmail.com>
2019-04-14 02:52:36 +0530

    adds signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_user, except: [:show, :index, :new]
+	before_action :require_user, except: [:show, :index, :new, :create]
 	before_action :require_same_user, only: [:update, :edit, :destroy]
 
 	def new
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "User was succesfully created"
 			redirect_to user_path(@user)
 		else
